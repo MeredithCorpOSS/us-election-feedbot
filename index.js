5de7c3c6c85c0173d6466359467c1cbbd076e5e4
Prathik Shetty <iprathik@gmail.com>
2016-11-03 19:47:46 +0530

    #0 url change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ bot.dialog('/', function (session) {
 
 // Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', connector.listen());
+server.post('/', connector.listen());
 server.listen(process.env.PORT || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
