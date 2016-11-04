cd9b04b26114d045a859df1016a9482e5fe51013
Prathik Shetty <iprathik@gmail.com>
2016-11-04 14:27:11 +0530

    changes

--- sources/twitter/index.js
+++ sources/twitter/index.js
@@ -1,68 +1,45 @@
 'use strict';
 const handler = require('./makeCall');
+const builder = require('botbuilder');
+const botSender = require('../../botSender.js');
+const users = require('../../users.js');
 
 
 function lambdaHandler(event, context, cb) {
 
-  try {
-    var resourcePath = event.resource_path;
-    console.log(resourcePath);
-    // validator.validateField(resourcePath, 'Resource Path is Null');
+    try {
+        var resourcePath = event.resource_path;
+        console.log(resourcePath);
+        // validator.validateField(resourcePath, 'Resource Path is Null');
 
-    // var reqHandler = new IAsset.AssetRequestHandler();
+        // var reqHandler = new IAsset.AssetRequestHandler();
 
-    handler.get(event, context, function(res){
-      
-      if(!res) {
-        return false;
-      }
+        handler.get(event, context, function (res) {
 
-      var x, msg;
-      var restify = require('restify');
-      var builder = require('botbuilder');
-      var config  = require('../../config');
+            if (!res) {
+                return false;
+            }
 
-      // Create bot and add dialogs
-      var connector = new builder.ChatConnector({
-        appId: config.microsoftBot.appId,
-        appPassword: config.microsoftBot.appSecret
-      });
+            users.getEach(function (user) {
+                let x;
+                let msg;
 
-      var address = { 
-        id: '1ObYpBk6jEc|000000000000000004',
-        channelId: 'webchat',
-        user: { id: 'BPnto7CIh1z' },
-        conversation: { id: '1ObYpBk6jEc' },
-        bot: { id: 'Timeincbot', name: 'Timeincbot' },
-        serviceUrl: 'https://webchat.botframework.com',
-        useAuth: true 
-      }
+                for (x in res) {
+                    msg = new builder.Message()
+                        .address(user)
+                        .text(res[x].text);
+                }
+            });
 
-
-
-      var connector = new builder.ChatConnector({
-        appId: config.microsoftBot.appId,
-        appPassword: config.microsoftBot.appSecret
-      });
-
-      var bot = new builder.UniversalBot(connector); 
-
-      for (x in res){
-        msg = new builder.Message()
-          .address(address)
-          .text(res[x].text);
-        bot.send(msg);
-        console.log(res[x].text);
-      }
-    });
-  } catch (err) {
-    console.log('Inside Index.js Catch: Caught Error :' + err);
-    var error = {
-      'ok': 'false11',
-      'message': err.message
-    };
-    cb(JSON.stringify(error));
-  }
+        });
+    } catch (err) {
+        console.log('Inside Index.js Catch: Caught Error :' + err);
+        var error = {
+            'ok': 'false11',
+            'message': err.message
+        };
+        cb(JSON.stringify(error));
+    }
 }
 
 exports.handler = lambdaHandler;
